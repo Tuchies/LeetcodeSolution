9c8949ddfe2bbca2fb1a05e81920f6b8f51f8fef
PusenYang <pusenyang@outlook.com>
2020-07-08 14:14:25 +0800

    补充了部分题目

--- Leetcode题解/L017.cpp
+++ Leetcode题解/L017.cpp
@@ -1,17 +1,48 @@
 
 // 根据九宫格布局输出电话号码的可能组合
-// 回溯法递归求解，接收两个参数：1.当前数字已经组成的对应字符串，2.剩余的数字字符串；返回条件：剩余的字符串为空，即所有的数字都匹配完；定义一个全局list存放可能的结果，当一个分支完整地遍历之后记录结果
+/**
+ * 解题思路: 参见: https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/solution/tong-su-yi-dong-dong-hua-yan-shi-17-dian-hua-hao-m/
+ * 
+ * 回溯返回条件: index = digits.size(), 即digits全部被扫描
+ * 循环的过程中: 
+ * 
+ */
 
 #include<iostream>
 #include<vector>
-#include<map>
+#include<string>
 
 using namespace std;
 
+vector<string> ans; // 结果数组, 设为全局变量
+vector<string> numberList={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"}; //字符表，错位使号码和索引对应
+    
+vector<string> letterCombinations(string digits) {
+    if(digits.size()==0) return {};  // 特判
+    helper(digits, {}, 0);
+    return ans;
+}
 
-
+void helper(string digits, string s, int idx){
+    if(idx == digits.size()){ // 回溯条件，保证digits全部被扫描
+        ans.push_back(s);   // 将一种结果压入ans
+        return;
+    }
+    else{
+        // 依据digits的位数进行迭代，例如digits="234"
+        // 第一层迭代为"2"，将遍历对应的字符串"abc"并更新字符串，依次为"a","b","c"
+        // 第二层迭代为"3"，将遍历对应的字符串"def"并更新字符串，若上一层迭代为"a"，则添加、更新为"ad", "ae"与"af".
+        int pos = digits[idx]-'0';  // 获取idx在digits的字符,如“2”对应的2
+        string tmpStr = numberList[pos];  // 获取下标pos对应的字符串，如2对应的"abc"
+        for(int i = 0;i < tmpStr.size(); i++){
+            helper(digits, s+tmpStr[i], idx+1);   // 进行下一层迭代，注意同一层迭代时不改变s和idx等参数值
+        }
+    }
+}
 
 int main() {
-   
+    string ins;
+    cin >> ins;
+    vector<
     return 0;
 }
---
+++ 剑指Offer/树/重建二叉树/Main.cpp
@@ -0,0 +1,56 @@
+/**
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+ */
+
+/**
+ * 参考: https://www.nowcoder.com/questionTerminal/8a19cbe657394eeaac2f6ea9b0f6fcf6?f=discussion
+ * 
+ * 思路解析: 
+ * 1. 前序遍历序列的第一个数字是根节点的值, 对应的在中序遍历序列的中间, 由此确定根节点, 以及左右子树节点的个数和值
+ * 2. 对于根节点划分来的左右子树, 递归的创建它们的左右子树
+ */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x),left(NULL),right(NULL){}
+};
+
+TreeNode* helper(vector<int> pre,int startPre,int endPre,vector<int> vin,int startVin,int endVin);
+
+TreeNode* reConstructBinaryTree(vector<int> pre,vector<int> vin) {
+    TreeNode* root = helper(pre,0,pre.size()-1,vin,0,vin.size()-1);
+    return root;
+}
+
+TreeNode* helper(vector<int> pre,int startPre,int endPre,vector<int> vin,int startVin,int endVin) {
+    // 边界判断
+    if(startPre > endPre || startVin > endVin)
+        return NULL;
+    // 1.初始化根节点: 永远是前序遍历的首元素    
+    TreeNode* root = new TreeNode(pre[startPre]);
+    // 2.遍历中序遍历, 区分左右子树    
+    for(int i = startVin; i <= endVin; i++)
+        if(vin[i] == pre[startPre]) {
+            // 2.1左子树递归创建
+            root->left = helper(pre,startPre+1,startPre+i-startVin,vin,startVin,i-1);
+            // 2.2右子树递归创建
+            root->right= helper(pre,i-startVin+startPre+1,endPre,vin,i+1,endVin);
+            break;
+        }
+                
+    return root;
+}
+
+int main() {
+    vector<int> pre = {1,2,4,7,3,5,6,8};
+    vector<int> vin = {4,7,2,1,5,3,8,6};
+    cout << reConstructBinaryTree(pre,vin)->val << endl;
+    return 0;
+}
+
